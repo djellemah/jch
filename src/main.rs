a13838e93ce58e263f91db1771d8bc6cd4fb067c
John Anderson <panic@semiosix.com>
2023-08-02 18:48:47 -0400

    clean up the generators and iterator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-#![feature(generators, generator_trait)]
-// https://serde.rs/stream-array.html
-// seems to be a bit clunky
-
-// actually designed for streaming, but seems a bit incomplete
-// https://github.com/Marcono1234/struson
-
 // type StrCon<T> = std::rc::Rc<T>;
 type StrCon<T> = Box<T>;
 
@@ -27,8 +20,7 @@ struct JsonEvents {
 }
 
 impl JsonEvents {
-  fn new() -> Self {
-    let istream = make_readable();
+  fn new(istream : StrCon<dyn std::io::BufRead>) -> Self {
     let reader = json_event_parser::JsonReader::from_reader(istream);
     let buf : Vec<u8> = vec![];
     Self{reader, buf}
@@ -42,52 +34,10 @@ impl JsonEvents {
   }
 }
 
-// fn make_json_event_iterator<'l>(istream : &'l mut impl std::io::BufRead) -> impl std::iter::Iterator + 'l {
-// fn make_json_event_iterator<'l>() -> impl std::iter::Iterator + 'l {
-//   let istream = make_readable();
-//   let mut reader = json_event_parser::JsonReader::from_reader(istream);
-//   let mut buf : Vec<u8> = vec![];
-
-//   std::iter::from_fn( move || {
-//     match reader.read_event(&mut buf).unwrap() {
-//       eof @ json_event_parser::JsonEvent::Eof => None,
-//       event => Some(event),
-//     }
-//   })
-// }
-
 fn main() {
-  // let istream = make_readable();
-  // json_event_parser::{JsonReader, JsonEvent};
-  // use std::io::Cursor;
-  // use std::io::BufRead;
-
-
-  // let mut reader = json_event_parser::JsonReader::from_reader(Cursor::new(istream));
-  // let mut reader = json_event_parser::JsonReader::from_reader(istream);
-  let mut json_events = JsonEvents::new();
+  let istream = make_readable();
+  let mut json_events = JsonEvents::new(istream);
   while let Some(ev) = json_events.next() {
     println!("{ev:?}");
   }
-
-  // let mut generator = move || {
-  //   let mut buf : Vec<u8> = vec![];
-  //   loop {
-  //     match reader.read_event(&mut buf).unwrap() {
-  //       eof @ json_event_parser::JsonEvent::Eof => return eof,
-  //       event => {
-  //         yield event
-  //       }
-  //     };
-  //   }
-  // };
-
-  // use std::ops::Generator;
-  // loop {
-  //   match std::pin::Pin::new(&mut generator).resume(()) {
-  //     std::ops::GeneratorState::Yielded(_jason_event) => (),
-  //     std::ops::GeneratorState::Complete(json_event_parser::JsonEvent::Eof) => (),
-  //     other => panic!("unexpected yield {other:?}"),
-  //   }
-  // }
 }
