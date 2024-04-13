cd6e494aa8d6664916097fdce21f882d99c44ee2
John Anderson <panic@semiosix.com>
2024-04-13 08:58:50 -0400

    revert to sending non-rpds Vec

--- src/sendpath.rs
+++ src/sendpath.rs
@@ -2,27 +2,34 @@
 // channel without excessive copying.
 
 use crate::jsonpath::JsonPath;
+use crate::jsonpath::Step;
 
-// At one point, this was also implemented in terms of
-// rpds::Vector. This may have been faster?
-#[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Clone)]
-pub struct SendPath(pub JsonPath);
+mod like_jsonpath {
+  // rpds needs a special constructor for this, so that we don't run into trouble
+  // with RcK in archery being not thread-safe.
+  use super::JsonPath;
 
-impl From<&JsonPath> for SendPath {
-  fn from(jsonpath : &JsonPath) -> Self {
-    Self(jsonpath.clone())
+  #[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Clone)]
+  pub struct SendPath(pub JsonPath);
+
+  impl From<&JsonPath> for SendPath {
+    fn from(jsonpath : &JsonPath) -> Self {
+      Self(jsonpath.clone())
+    }
   }
 }
 
 // a tree path as sent by the streaming parser to a handler of some kind, along with its leaf value.
-// struct SendPath(Vec<Step>);
-// impl From<&JsonPath> for SendPath {
-//   fn from(path_list : &JsonPath) -> Self {
-//     let steps = path_list.iter().map(std::clone::Clone::clone).collect::<Vec<Step>>();
-//     // steps.reverse(); for list
-//     Self(steps)
-//   }
-// }
+#[derive(Debug,Clone)]
+pub struct SendPath(pub Vec<Step>);
+
+impl From<&JsonPath> for SendPath {
+  fn from(path_list : &JsonPath) -> Self {
+    let steps = path_list.iter().map(std::clone::Clone::clone).collect::<Vec<Step>>();
+    // steps.reverse(); for list
+    Self(steps)
+  }
+}
 
 // This produces jq-equivalent notation
 impl std::fmt::Octal for SendPath {
