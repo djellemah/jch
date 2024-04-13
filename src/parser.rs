6766d0417d72a57bc3bd0f8c4943e76de233e356
John Anderson <panic@semiosix.com>
2024-04-13 09:43:06 -0400

    try to rectify next/nextbuf

--- src/parser.rs
+++ src/parser.rs
@@ -6,34 +6,19 @@ pub struct JsonEvents {
   _buf : Vec<u8>,
 }
 
-impl JsonEvents {
-  pub fn new(istream : StrCon<dyn std::io::BufRead>) -> Self {
-    let counter = Box::new(countio::Counter::new(istream));
-    // let rcounter = &counter;
-    let reader = json_event_parser::JsonReader::from_reader(counter);
-    let buf : Vec<u8> = vec![];
-    // Self{reader, counter: rcounter, buf}
-    Self{reader, _buf: buf}
-  }
-
-  // This is an attempt to use JsonEvents as an iterator.
-  // But it's a severe PITA to specify this as an implementation of Iterator
-  // TODO move error handling into next_buf
-  #[allow(dead_code)]
-  fn next(&mut self) -> Option<json_event_parser::JsonEvent> {
-    match self.reader.read_event(&mut self._buf) {
-      Ok(ev) => match ev {
-        json_event_parser::JsonEvent::Eof => None,
-        event => Some(event),
-      }
+// Sheesh. Trying to this with lifetimes is *severe* PITA
+macro_rules! eventicize {
+  ($obj:expr, $event_result:expr) => {
+    match $event_result {
+      Ok(json_event_parser::JsonEvent::Eof) => None,
+      Ok(jev) => Some(jev),
       Err(err) => {
-        let counter = &self.reader.reader as &countio::Counter<Box<dyn std::io::BufRead>>;
+        let counter = &$obj.reader.reader as &countio::Counter<Box<dyn std::io::BufRead>>;
         let pos = counter.reader_bytes();
-        // let mut buf : Vec<u8> = Vec::with_capacity(25);
-        // try to generate some context
+        // try to generate some surrounding json context for the error message
         let mut buf = [0; 80];
         use std::io::Read;
-        let more = match self.reader.reader.read(&mut buf) {
+        let more = match $obj.reader.reader.read(&mut buf) {
           Ok(n) => String::from_utf8_lossy(&buf[0..n]).to_string(),
           Err(err) => format!("{err:?}"),
         };
@@ -45,12 +30,29 @@ impl JsonEvents {
       }
     }
   }
+}
+
+impl JsonEvents {
+  pub fn new(istream : StrCon<dyn std::io::BufRead>) -> Self {
+    let counter = Box::new(countio::Counter::new(istream));
+    // let rcounter = &counter;
+    let reader = json_event_parser::JsonReader::from_reader(counter);
+    let buf : Vec<u8> = vec![];
+    // Self{reader, counter: rcounter, buf}
+    Self{reader, _buf: buf}
+  }
+
+  // This is an attempt to use JsonEvents as an iterator.
+  // But it's a severe PITA to specify this as an implementation of Iterator,
+  // and it's impossible to pass errors back.
+  #[allow(dead_code)]
+  fn next(&mut self) -> Option<json_event_parser::JsonEvent> {
+    let event_result = self.reader.read_event(&mut self._buf);
+    eventicize!(self, event_result)
+  }
 
   pub fn next_buf<'a, 'b>(&'a mut self, buf : &'b mut Vec<u8>) -> Option<json_event_parser::JsonEvent<'b>> {
-    match self.reader.read_event(buf) {
-      Ok(json_event_parser::JsonEvent::Eof) => None,
-      Ok(event) => Some(event),
-      Err(_) => None,
-    }
+    let event_result = self.reader.read_event(buf);
+    eventicize!(self, event_result)
   }
 }
